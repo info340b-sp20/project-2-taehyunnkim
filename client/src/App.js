93c5b15dbd548bfa9759a12fa9b77a9a84e84ccb
anie0521 <anie0521@uw.edu>
2020-05-29 14:40:11 -0700

    add navbar component, need to fix reactstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
     </div>
   );
 }
+
+export class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <ul id="nav">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#donate">Donate</a></li>
+          <li><a href="#signin">Sign in</a></li>
+        </ul>
+      </div>
+    )
+  }
+}
+
 
 export default App;
