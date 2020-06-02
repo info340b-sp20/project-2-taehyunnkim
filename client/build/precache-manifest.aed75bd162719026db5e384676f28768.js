9d85e6ca4f1a6216e076ab46d7a11a0c9d1bb03f
Eric Kim <kim2000@uw.edu>
2020-06-02 14:09:16 +0900

    Add new build

---
+++ client/build/precache-manifest.aed75bd162719026db5e384676f28768.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dc0ff4e7cab7552e30695fc14ebf4f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85ddf9866ba4169023f1",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8be3d3ef1732cb5f0860",
+    "url": "/static/css/main.6f29ace9.chunk.css"
+  },
+  {
+    "revision": "85ddf9866ba4169023f1",
+    "url": "/static/js/2.3d92a2ef.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.3d92a2ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8be3d3ef1732cb5f0860",
+    "url": "/static/js/main.3a99e1de.chunk.js"
+  },
+  {
+    "revision": "f3a87a1cf86047070d79",
+    "url": "/static/js/runtime-main.315bf881.js"
+  }
+]);
